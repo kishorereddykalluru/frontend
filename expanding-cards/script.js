92f4cd214c67dddd00d92b158e501c525c435b70
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-07-01 14:19:00 +0530

    project starter and expanding cards

---
+++ expanding-cards/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeAllActivePanels()
+        panel.classList.add('active')
+    })
+})
+
+function removeAllActivePanels(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
